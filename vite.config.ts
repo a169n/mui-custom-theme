097a7faa206fc32ace43043f6f0c75e0667f1171
Aibyn <talgatov_a@bi.group>
2025-10-28 16:35:53 +0500

    Update .gitignore and modify Vite server port configuration
    - Expanded .gitignore to include additional files and directories for better project cleanliness, such as build outputs, environment variables, IDE settings, OS-specific files, logs, and temporary files.
    - Changed Vite server port from 3000 to 3001 to avoid potential conflicts.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Listen on all network interfaces (0.0.0.0)
-    port: 3000,
+    port: 3001,
     open: true,
     hmr: true,
   },
